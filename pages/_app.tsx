12bfcddd90864ba972c3026c6a7e971e8cd73bba
Sk Singh <129384571+0xsksingh@users.noreply.github.com>
2024-03-15 10:32:35 +0530

    setting up thirdweb with botanix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,31 @@
-import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { ThirdwebProvider } from "@thirdweb-dev/react";
+import { Navbar } from "../components/Navbar/Navbar";
+import NextNProgress from "nextjs-progressbar";
+import { Botanix, BotanixTestnet } from "@thirdweb-dev/chains";
+import "../styles/globals.css";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ThirdwebProvider
+      activeChain={BotanixTestnet}
+      clientId={process.env.NEXT_PUBLIC_CLIENT_ID}
+    >
+      {/* Progress bar when navigating between pages */}
+      <NextNProgress
+        color="var(--color-tertiary)"
+        startPosition={0.3}
+        stopDelayMs={200}
+        height={3}
+        showOnShallow={true}
+      />
+
+      {/* Render the navigation menu above each component */}
+      <Navbar />
+      {/* Render the actual component (page) */}
+      <Component {...pageProps} />
+    </ThirdwebProvider>
+  );
 }
+
+export default MyApp;
