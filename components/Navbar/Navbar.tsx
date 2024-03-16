238eaba218ca0c8ebdca4fc21ce23caa7da3a728
Sk Singh <129384571+0xsksingh@users.noreply.github.com>
2024-03-16 12:36:32 +0530

    NFT component

---
+++ components/Navbar/Navbar.tsx
@@ -0,0 +1,65 @@
+import { ConnectWallet, useAddress } from "@thirdweb-dev/react";
+import Image from "next/image";
+import Link from "next/link";
+import styles from "./Navbar.module.css";
+
+export function Navbar() {
+  const address = useAddress();
+
+  return (
+    <div className={styles.navContainer}>
+      <nav className={styles.nav}>
+        <div className={styles.navLeft}>
+          <Link href="/" className={`${styles.homeLink} ${styles.navLeft}`}>
+            <Image
+              src="/marketplace.png"
+              width={48}
+              height={48}
+              alt="NFT marketplace sample logo"
+            />
+          </Link>
+
+          <div className={styles.navMiddle}>
+            <Link href="/buy" className={styles.link}>
+              Buy
+            </Link>
+            <Link href="/sell" className={styles.link}>
+              Sell
+            </Link>
+            <Link href="/rent" className={styles.link}>
+              Rent
+            </Link>
+          </div>
+        </div>
+
+        <div className={styles.navRight}>
+          <div className={styles.navConnect}>
+            <ConnectWallet theme="dark" btnTitle={"Enter BotanixNFTHub"} 
+            welcomeScreen={{
+              img: {
+                src: "https://pbs.twimg.com/profile_images/1668751547285504002/Fn4GgdKT_400x400.jpg",
+                width: 150,
+                height: 150,
+              },
+              title:
+                "Your Entry to the world of Botanix",
+              subtitle: "Connect Wallet to enter 🎉",
+            }}
+            />
+          </div>
+          {address && (
+            <Link className={styles.link} href={`/profile/${address}`}>
+              <Image
+                className={styles.profileImage}
+                src="/user-icon.png"
+                width={42}
+                height={42}
+                alt="Profile"
+              />
+            </Link>
+          )}
+        </div>
+      </nav>
+    </div>
+  );
+}
---
+++ const/contractAddresses.ts
@@ -0,0 +1,11 @@
+import { BotanixTestnet } from "@thirdweb-dev/chains";
+
+export const NETWORK = BotanixTestnet;
+
+export const MARKETPLACE_ADDRESS = "0x07E303007355dFc8AC691bDc19C0787f775E91da";
+
+export const NFT_COLLECTION_ADDRESS = "0x3854AECCFacb32A828632276ed2744CEC48CEE26";
+
+export const RENTNFT_COLLECTION_ADDRESS = "";
+
+export const EXPLORER_URL = "https://blockscout.botanixlabs.dev/";
